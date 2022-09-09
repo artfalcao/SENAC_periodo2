2e6a704b5fcb83681fa13e42dd5d729a440948e3
Arthur Monteiro Falcão <95629281+artfalcao@users.noreply.github.com>
2022-09-09 09:35:42 -0300

    Add files via upload

---
+++ CODING/Pratica2/Pratica2.1/script.js
@@ -0,0 +1,39 @@
+function click1() {
+    let main = document.getElementById("main")
+    main.innerHTML = ''
+    main.innerHTML +=  `
+        <iframe src="./sobreMim.html" frameborder="1" style="height: 40vh;"></iframe>
+    `   
+}
+
+function click2() {
+    let main = document.getElementById("main")
+    main.innerHTML = ''
+    main.innerHTML +=  `
+        <iframe src="./formacao.html" frameborder="1" style="height: 40vh;"></iframe>
+    `  
+}
+
+function click3() {
+    let main = document.getElementById("main")
+    main.innerHTML = ''
+    main.innerHTML +=  `
+        <iframe src="./experiencia.html" frameborder="1" style="height: 40vh;"></iframe>
+    `  
+}
+
+function click4() {
+    let main = document.getElementById("main")
+    main.innerHTML = ''
+    main.innerHTML +=  `
+        <iframe src="./cursos.html" frameborder="1" style="height: 50vh;"></iframe>
+    `  
+}
+
+function click5() {
+    let main = document.getElementById("main")
+    main.innerHTML = ''
+    main.innerHTML +=  `
+    <iframe src="./habilidades.html" frameborder="1" style="height: 40vh;"></iframe>
+    `  
+}
