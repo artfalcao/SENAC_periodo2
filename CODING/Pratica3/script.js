1e98c7863e3092841e9b86294cbd9641a6d98264
Arthur Monteiro Falcão <95629281+artfalcao@users.noreply.github.com>
2022-10-06 11:36:21 -0300

    Create script.js

---
+++ CODING/Pratica3/script.js
@@ -0,0 +1,22 @@
+const elements = {
+    nome : document.getElementById("nome"),
+    sobrenome: document.getElementById("sobrenome"),
+    telefone: document.getElementById("tel"),
+    email: document.getElementById("email"),
+    mensagem: document.getElementById("msg"),
+    lingua: document.getElementById("lingua"),
+    proposito: document.getElementById("file"),
+    linguagem: document.getElementById("linguagem"),
+    db: document.getElementsByName("db"),
+    turnoReuniao: document.getElementsByName("turno_reuniao")
+}
+
+
+
+function saveData() {
+
+}
+
+function clearData() {
+
+}
