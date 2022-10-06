ca513e0d9bfaeabae1f3c0c6bce1ca467196db06
Arthur-Falcao <arthurmonteirofalcao1995@gmail.com>
2022-10-06 16:49:40 -0300

    Saving data on localStorage

--- CODING/Pratica3/script.js
+++ CODING/Pratica3/script.js
@@ -8,13 +8,37 @@ const elements = {
     proposito: document.getElementById("file"),
     linguagem: document.getElementById("linguagem"),
     db: document.getElementsByName("db"),
-    turnoReuniao: document.getElementsByName("turno_reuniao")
+    turnoReuniao: document.getElementsByName("turno_reuniao"),
+    horario: document.getElementById("horario"),
+    data: document.getElementById("data")
 }
 
 
-
 function saveData() {
+    //Salvando alguns dados
+    let nome = elements.nome.value
+    let sobrenome = elements.sobrenome.value
+    let lingua = elements.lingua.value
+    let horario = elements.horario.value
+    let db = elements.db
+    let dbValue;
+    //Para saber qual radioButton foi selecionado
+    for (let i = 0; i < db.length; i++){
+        if(db[i].checked){
+            dbValue = db[i].value;
+        }
+    }
 
+    let freela = {
+        "nome": nome,
+        "sobrenome": sobrenome,
+        "lingua": lingua,
+        "horario": horario,
+        "db": dbValue
+    }
+    
+    //Salvando no localStorage
+    localStorage.setItem(`${nome} ${sobrenome}`, JSON.stringify(freela))
 }
 
 function clearData() {
